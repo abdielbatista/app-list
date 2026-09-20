6661f6bc42402adc11c7f7b0f4297118cda2b5b2
Abdiel Batista dos Santos <abdielb@yahoo.com>
2026-09-20 20:53:19 -0300

    criação de routes

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Login from './src/pages/login';
+import './gesture-handler';
 
 
 export default function App() {
